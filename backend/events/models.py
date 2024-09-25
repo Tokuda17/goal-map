50cd4b46d8a0285668bdc28dc8c7a3b4ce61e97a
Michael Tokuda <michael.tokuda17@gmail.com>
2024-09-24 23:17:46 -0400

    workign more on django tutorial

--- backend/events/models.py
+++ backend/events/models.py
@@ -7,3 +7,6 @@ class Event(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
+
+    def __str__(self):
+        return self.description
--- backend/events/views.py
+++ backend/events/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Event
 
 
 def index(request):
-    return HttpResponse("Hello World. This is the Event Index")
+    events = Event.objects.all()
+    print(events)
+    
+    return render(request, "events/index.html", {"events": events})
 
 def tester(requet):
     return HttpResponse("Hello Tester")
