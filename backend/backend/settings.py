a0a3ee67aa03b1c7d93bbc72d946a1c900c9b1b8
Michael Tokuda <michael.tokuda17@gmail.com>
2024-12-28 00:52:49 -1000

    settign changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,12 +108,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': ''
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
